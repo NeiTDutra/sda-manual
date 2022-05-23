d4d3776d4da28142750de35664ebcea50ff6d3a2
nei <nei.thomass@gmail.com>
2022-05-23 11:40:04 -0300

    implementa ações de menu

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+$('nav a').click(function(e){
+    e.preventDefault;
+    var href = $(this).attr('href'),
+        targetOffset = $(href).offset().top,
+        headerHeight = $('header').innerHeight() + 35;
+    $('html, body').animate({
+        scrollTop: targetOffset - headerHeight
+    }, 500)
+});
