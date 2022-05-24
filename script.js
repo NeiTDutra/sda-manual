7c8baeff431fc083c7bdea3c1e99cf510a5cc99c
nei <nei.thomass@gmail.com>
2022-05-23 21:01:18 -0300

    Ajusta o layout

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $('nav a').click(function(e){
     e.preventDefault;
     var href = $(this).attr('href'),
         targetOffset = $(href).offset().top,
-        headerHeight = $('header').innerHeight() + 35;
+        headerHeight = $('header').innerHeight() + 45;
     $('html, body').animate({
         scrollTop: targetOffset - headerHeight
     }, 500)
