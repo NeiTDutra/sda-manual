ef24d224d801aa8e6851230a09f14a325a214b12
nei <nei.thomass@gmail.com>
2022-05-27 08:38:26 -0300

    implementa envio de email

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ $('nav a, #content a').click(function(e){
         scrollTop: targetOffset - headerHeight
     }, 1000)
 });
+
+function sendMail() {
+
+    let name = document.querySelector('input[name="contact-name"]').value
+    let from = document.querySelector('input[name="contact-email"').value
+    let subject = document.querySelector('input[name="contact-title"]').value
+    let body = document.querySelector('textarea[name="contact-message"]').value
+
+    console.log(name+'\n'+from+'\n'+subject+'\n'+body)
+ 
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "sda.sys.2022@gmail.com",
+        Password : "Senha@sdasys#2022",
+        To : "sda.sys.2022@gmail.com",
+        From : from,
+        Subject : subject,
+        Body : body+'\n Ass. '+name
+    }).then(
+      message => alert('E-mail enviado!')
+    );
+    document.querySelector('input, textarea').reset()
+}
